28c391010dca35a1c975c7a51b235f4da77a4bed
rakib lanovo office <rfinfo247@gmail.com>
2021-05-04 17:14:13 +0600

    gateway add

---
+++ Stock-api-gateway/src/main/java/com/rhmtech/gateway/api/StockApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.rhmtech.gateway.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockApiGatewayApplication.class, args);
+	}
+
+}
